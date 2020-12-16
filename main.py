f671fe6f6a505bd2bf271b6811c1e385384cec4d
James Westwood <james.westwood@ons.gov.uk>
2020-12-16 16:48:13 +0000

    linted the code and made sure it is working for the 2 sample csvs

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-
+# Author: James Westwood
+# Copyright: Office for National Statistics
 # coding: utf-8
-# -*- coding: utf-8 -*-
-#
-# imports
-import pandas as pd
-import requests
-import re
-import os
-from yaml import safe_load, dump
-from bs4 import BeautifulSoup as bs
-import random
-from modules import write_csv, standardise_cell_values, fill_gaps, csvs_to_pandas, find_csv_urls, get_mapping_dicts, override_writer
 
+# imports
+import os
+import re
+
+
+from modules import (csvs_to_pandas,
+                     get_mapping_dicts, 
+                     override_writer,
+                     write_csv)
 
 """
 sdg-csv-data-filler is the first module in a data pipeline to take
@@ -23,15 +22,16 @@ remote_data_url = "https://github.com/ONSdigital/sdg-data/tree/develop/data"
 cwd = os.getcwd()
 data_path = os.path.join(cwd, 'data')
 out_path = os.path.join(cwd, 'out')
-overrides_yam = (os.path.join(cwd,"overrides_dict.yaml"))
+overrides_yam = (os.path.join(cwd, "overrides_dict.yaml"))
 
 POC3_urls = ['https://raw.githubusercontent.com/ONSdigital/sdg-data/develop/data/indicator_11-7-1.csv',
-            'https://raw.githubusercontent.com/ONSdigital/sdg-data/develop/data/indicator_16-9-1.csv']
+                'https://raw.githubusercontent.com/ONSdigital/sdg-data/develop/data/indicator_16-9-1.csv']
 
 
 def entry_point(data_url):
 
-    # define pattern for name matching outside the for-loop. Used for writing out later
+    # define pattern for name matching outside the for-loop.
+    # pattern used for writing out later
     pattern = r"(indicator_\d{1,2}-\d{1,2}-\d+\.csv)$"
 
     # create an empty results dict
@@ -42,22 +42,19 @@ def entry_point(data_url):
         overrides_dict = get_mapping_dicts(overrides_yam, _url)
         # Create df
         df = csvs_to_pandas(_url)
-        #get dataset name
+        # get dataset name
         file_name = f"{re.search(pattern, _url).group(0)}"
-        if df is None or df.empty: # sometimes no df will be returned so it needs to be skipped
+        if df is None or df.empty:  # sometimes no df will be returned
             results[file_name] = False
-            continue
-        # Apply transformations to the df 
+            continue  # empty dfs are skipped
+        # Apply transformations to the df
         df = override_writer(df, overrides_dict)
-        #Writing the df to csv locally. 
+        # Writing the df to csv locally
         was_written = write_csv(df, out_path, file_name)
         results[file_name] = was_written
 
     return results
 
 
-results = entry_point(data_url=remote_data_url)
-
-
-# print(f"number of CSVs missing from output = {len(list(find_csv_urls(remote_data_url)))-len(results.keys())}")
-
+if __name__ == "__main__":
+    results = entry_point(data_url=remote_data_url)
--- modules.py
+++ modules.py
@@ -1,32 +1,40 @@
+# Author: James Westwood
+# Copyright: Office for National Statistics
+# coding: utf-8
+
+# Core imports
+import os
+import random
+import re
+
+# Third party imports
+import numpy as np
 import pandas as pd
 import requests
-import re
-import os
-from yaml import safe_load, dump
 from bs4 import BeautifulSoup as bs
-import random
-import numpy as np
+from yaml import safe_load
+
 
 def get_mapping_dicts(path_to_yaml, url):
     """
-    Loads dictionaries for the gap filling and mapping of column names 
-    from locally stored .yaml files. Returns only the dictionary for 
+    Loads dictionaries for the gap filling and mapping of column names
+    from locally stored .yaml files. Returns only the dictionary for
     the specified dataset as the url is the unique identifier for the dataset.
     This unique identifier is used as a key on the returned dictionary.
-    If the unique identifier (key) does not exist, `none` is returned. 
-    
+    If the unique identifier (key) does not exist, `none` is returned.
+
         Parameters:
             path_to_yaml (string): Path to the yaml file storing the values
                 to override values with
-            url (string): The source url for the dataset which is a unique identifier
+            url (string): The source url for the dataset which is a
+                unique identifier
         Returns:
             dict: dict_from_yam
-            
     """
     with open(path_to_yaml) as file:
         dict_from_yam = safe_load(file)
         if url not in dict_from_yam.keys():
-            print (f"{url} not in dictionary")
+            print(f"{url} not in dictionary")
         dict_from_yam = dict_from_yam.get(url)
     return dict_from_yam
 
@@ -40,14 +48,14 @@ def find_csv_urls(url):
             url (string): the URL of the repo/folder which contains
                 the CSV files to be captured
         Yields:
-            string: generator, the next URL for the CSV file in the 
-            remote data folder """
+            string: generator, the next URL for the CSV file in the
+            remote data folder"""
     page = requests.get(url)
     soup = bs(page.text, 'html.parser')
     csv_link_pattern = r"\/ONSdigital\/sdg-data\/blob\/develop\/data\/indicator_\d-\d{1,2}-\d{1,2}\.csv"
     to_repl_pattern = r"\/sdg-data\/blob\/develop"
     replacement_pattern = "/sdg-data/develop"
-    attrs={'href': re.compile(csv_link_pattern)}
+    attrs = {'href': re.compile(csv_link_pattern)}
     for link in soup.findAll('a', attrs=attrs):
         link = link.get('href')
         link = re.sub(to_repl_pattern, replacement_pattern, link)
@@ -55,42 +63,47 @@ def find_csv_urls(url):
 
 
 def csvs_to_pandas(url_to_csv):
-    """Provided with a URL of a file, the fucntion will check if the CSV
+    """Provided with a URL of a file, the function will check if the CSV
     is populated and if not empty return a Pandas dataframe of the CSV
         Parameters:
             url (string): the URL of a CSV file to be captured
         Returns:
             pd.DataFrame: a Pandas dataframe of the CSV
     """
-    if "no data for this indicator yet" in str(bs(requests.get(url_to_csv).text)):
+    str_response = str(bs(requests.get(url_to_csv).text, 
+                    features="html.parser"))
+    if "no data for this indicator yet" in str_response:
         return None
     else:
         return pd.read_csv(url_to_csv)
 
+# TODO make sure this is used
 def prevent_bad_replacement(overrides_dict, df):
-    """Checks that there is not any values equal to the placeholder-values which are used 
-        as keys in the overrides dict. If they did exist, this would cause a bad replacement.
+    """Checks that there is not any values equal to the "placeholder-values"
+        Placeholder values are used as keys in the overrides dict.
+        If they did exist in the dataframe this could cause a bad replacement.
         It is very unlikely that this is needed, but just here as an extra safety step"""
     place_holder_values = ['FILL_NA', 'OldValue1', 'OldValue2', 'OldValue3', 'to']
     results_dict = {}
     for search_value in place_holder_values:
         if search_value in df.values:
             results_dict[search_value] = True
-            raise Exception(f"Value {search_value} has been found in the dataframe")
+            raise Exception(f"""
+            Value {search_value} has been
+            found in the dataframe""")
     return None
 
 
 def csvsample_to_pandas(path_to_file, pct=1.0):
     """A function to create a sample extract of a csv as a dataframe
-    
         Parameters:
             path_to_file (string): full path to csv file
             p (float): decimal amount of lines to extract
-            
+
         Returns:
             pd.Dataframe
             """
-    p = pct/100  
+    p = pct/100
     # keep the header, then take only 1% of lines
     # if random from [0,1] interval is greater than 0.01 the row will be skipped
     df = pd.read_csv(
@@ -105,9 +118,9 @@ def fill_gaps(df, gap_filler_dict):
     the correct 'fillers' for each column, this function will fill
     each column with the correct values when empty cells are found.
         Parameters:
-            pd_df (pd.Dataframe): the variable to which the dataframe 
+            pd_df (pd.Dataframe): the variable to which the dataframe
                 containing the csv data is assigned
-            gap_filler_dict (dict): a dictionary with column name and value 
+            gap_filler_dict (dict): a dictionary with column name and value
                 to fill gaps as key value pairs, e.g.
                 {"Age":"All","Sex":"T"}
         Returns:
@@ -132,7 +145,7 @@ def write_csv(df, out_path, filename):
             pd_df (pd.Dataframe): The pandas data frame of the data
             path (string): the path of the local "out" folder
         Returns:
-            Boolean: True is written, False if not written """ 
+            Boolean: True if is written, False if not written"""
     status = True
 
     # If the out_path isn't there, make it
@@ -143,25 +156,29 @@ def write_csv(df, out_path, filename):
         full_path = os.path.join(out_path, filename)
         df.to_csv(full_path, index=False)
     except Exception:
-        extended_e = Exception(f"Error encountered when attempting csv write to {full_path}: ") #from e
+        extended_e = Exception(f"""
+        Error encountered when attempting csv write to {full_path}: """)
         print(extended_e)
-        return False
+        status = False
 
     return status
 
 def override_writer(df, overrides_dict):
-    """Takes the data frame and makes column-specific replacements or overrides. 
-        If fix_headers is True (False is default), it will change the headers to name in the overides dict. 
-        If standardise_cells is True (default), it will search for the value to be replaced and if found
-        the value will be replaced. If fill_gaps is True (default) it will fill any gaps with the replacement
-        value. 
-        
+    """Takes the data frame and makes column-specific replacements or
+        overrides. If fix_headers is True (False is default), it will
+        change the headers to name in the overides dict.
+        If standardise_cells is True (default), it will search for the value
+        to be replaced and if found value will be replaced.
+        If fill_gaps is True (default) it will fill any gaps with the replacement
+        or "gap filler" value.
+
         Parameters:
             df (pd.Dataframe): dataframe to be processed
-            overrides_dict (dict): The overrides dictionary specific to the dataset being processed
-            
+            overrides_dict (dict): The overrides dictionary specific to the
+                dataset being processed
+
         Returns:
-            pd.Dataframe: complete with requested value overrides 
+            pd.Dataframe: complete with requested value overrides
     """
     fix_headers = overrides_dict['fix_headers']
     standardise_cells = overrides_dict['standardise_cells']
@@ -171,7 +188,7 @@ def override_writer(df, overrides_dict):
         pass
     if standardise_cells:
         for column in df.columns:
-            if column in ['value','Value']: 
+            if column in ['value','Value']:
                 continue #skipping because Value is never a key in the dict
             orig_dtype = str(df[column].dtype)
             df[column] = df[column].astype(str)
@@ -179,10 +196,10 @@ def override_writer(df, overrides_dict):
             df[column] = df[column].astype(orig_dtype)
     if fill_gaps:
         for column in df.columns:
-            if column in ['value','Value']: 
+            if column in ['value','Value']:
                 continue #skipping because Value is never a key in the dict
-#             import ipdb; ipdb.set_trace()
-            df[column].replace('nan', np.nan, inplace=True) #replacing string 'nan' with numpy.nan
+            #replacing string 'nan' with numpy.nan
+            df[column].replace('nan', np.nan, inplace=True)
             df[column].fillna(
                 value=overrides_dict[column]['FILL_NA'],
                 inplace=True)
